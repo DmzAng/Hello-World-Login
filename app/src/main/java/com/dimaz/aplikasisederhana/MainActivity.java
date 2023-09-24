9431039c67030eaa36f4108eb55893b614b598d5
Dimaz Anugrahman Yusuf <dimazangrah64@gmail.com>
2023-09-24 14:23:16 +0800

    Second Commit

--- app/src/main/java/com/dimaz/aplikasisederhana/MainActivity.java
+++ app/src/main/java/com/dimaz/aplikasisederhana/MainActivity.java
@@ -2,6 +2,11 @@ package com.dimaz.aplikasisederhana;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn =(MaterialButton) findViewById(R.id.loginbtn);
+
+//      login test
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("Mahasiswa") && password.getText().toString().equals("123456")){
+//                    benar
+                    Toast.makeText(MainActivity.this, "Login Berhasil", Toast.LENGTH_SHORT).show(); }
+                else
+//                    salah
+                    Toast.makeText(MainActivity.this, "Login Gagal", Toast.LENGTH_SHORT).show();
+                }
+
+        });
     }
 }
